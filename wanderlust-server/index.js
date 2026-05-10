c795c96ae6ae546e83f37a8216825dd890bc306d
alfu-git <mdalfaz.dev@gmail.com>
2026-05-10 16:20:33 +0600

    setup backend server and connect with mongodb

---
+++ wanderlust-server/index.js
@@ -0,0 +1,44 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello from server");
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Server is running successfully on ${port}`);
+});
