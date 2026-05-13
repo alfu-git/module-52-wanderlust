8da5ed0e4f8599e75562fc316c93f29bfb17e63f
alfu-git <mdalfaz.dev@gmail.com>
2026-05-13 12:06:10 +0600

    configure better auth on client side

---
+++ wanderlust-client/src/lib/auth.js
@@ -0,0 +1,17 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+const client = new MongoClient(process.env.MONGODB_URI);
+const db = client.db("module_50_wanderlust-db");
+
+export const auth = betterAuth({
+  database: mongodbAdapter(db, {
+    client,
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+});
